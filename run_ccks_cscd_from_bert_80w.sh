efb3041282ad85c84402686d66a9b2c9e5430d5a
LeoWood <463861936@qq.com>
2020-03-13 11:30:33 +0800

    .

--- run_ccks_cscd_from_bert_80w.sh
+++ run_ccks_cscd_from_bert_80w.sh
@@ -9,14 +9,14 @@
     --data_dir=data/data_CCKS   \
     --vocab_file=/home/leo/lh/BERT/chinese_L-12_H-768_A-12/vocab.txt  \
     --bert_config_file=/home/leo/lh/BERT/chinese_L-12_H-768_A-12/bert_config.json \
-    --init_checkpoint=/home/leo/lh/BERT/chinese_L-12_H-768_A-12/bert_model.ckpt   \
+    --init_checkpoint=/home/leo/lh/Projects/bert/outputs/Pre2_cscd_R_128_from_bert/model.ckpt-800000   \
     --max_seq_length=128   \
     --train_batch_size=32   \
     --learning_rate=2e-5   \
-    --num_train_epochs=7.0   \
-    --output_dir=output/run_ccks_base \
+    --num_train_epochs=6.0   \
+    --output_dir=output/run_ccks_cscd_from_bert_80w \
     --middle_output=middle_data/ccks \
-    --gpu=0
+    --gpu=1
 
 
 #perl conlleval.pl -d '\t' < ./output/result_dir/label_test.txt
--- run_ccks_pre3_old.sh
+++ run_ccks_pre3_old.sh
@@ -9,14 +9,14 @@
     --data_dir=data/data_CCKS   \
     --vocab_file=/home/leo/lh/BERT/chinese_L-12_H-768_A-12/vocab.txt  \
     --bert_config_file=/home/leo/lh/BERT/chinese_L-12_H-768_A-12/bert_config.json \
-    --init_checkpoint=/home/leo/lh/BERT/chinese_L-12_H-768_A-12/bert_model.ckpt   \
+    --init_checkpoint=/home/leo/lh/Projects/bert/models/pre3_old   \
     --max_seq_length=128   \
     --train_batch_size=32   \
     --learning_rate=2e-5   \
-    --num_train_epochs=7.0   \
-    --output_dir=output/run_ccks_base \
+    --num_train_epochs=6.0   \
+    --output_dir=output/run_ccks_pre3_old \
     --middle_output=middle_data/ccks \
-    --gpu=0
+    --gpu=1
 
 
 #perl conlleval.pl -d '\t' < ./output/result_dir/label_test.txt
