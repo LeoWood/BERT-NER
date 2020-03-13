de67741f7192b0ddd04ab6c1e7efb7605c4accff
LeoWood <463861936@qq.com>
2020-03-13 13:35:10 +0800

    .

--- run_ccks_cscd_from_bert_80w.sh
+++ run_ccks_cscd_from_bert_80w.sh
@@ -9,7 +9,7 @@
     --data_dir=data/data_CCKS   \
     --vocab_file=/home/leo/lh/BERT/chinese_L-12_H-768_A-12/vocab.txt  \
     --bert_config_file=/home/leo/lh/BERT/chinese_L-12_H-768_A-12/bert_config.json \
-    --init_checkpoint=/home/leo/lh/Projects/bert/outputs/Pre2_cscd_R_128_from_bert/model.ckpt-800000   \
+    --init_checkpoint=/home/leo/lh/Projects/bert/outputs/Pre2_cscd_R_128_from_bert/model.ckpt-2000000   \
     --max_seq_length=128   \
     --train_batch_size=32   \
     --learning_rate=2e-5   \
